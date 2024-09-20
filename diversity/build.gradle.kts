1b17ede3151f52f49c29721e7867f09b4487301d
Molnár Botond <mbotond63@gmail.com>
2024-09-20 09:34:43 +0200

    Build conventions using kotlin-dsl

---
+++ diversity/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    id("hu.bme.mit.ase.shingler.gradle.java")
+}
+
+val picoCliVersion = "4.7.6"
+val slf4jVersion = "1.7.36"
+val log4jVersion = "2.23.1"
+val junitVersion = "5.10.0"
+
+dependencies {
+    implementation(project(":lib"))
+    implementation(project(":logic"))
+
+    implementation("org.slf4j:slf4j-api:$slf4jVersion")
+    implementation("info.picocli:picocli:$picoCliVersion")
+
+    runtimeOnly("org.apache.logging.log4j:log4j-slf4j-impl:$log4jVersion")
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
+
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
+}
+
+application {
+    mainClass = "hu.bme.mit.ase.shingler.diversity.SimilarityApp"
+}
--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -1,15 +1,5 @@
 plugins {
-    java
-    jacoco
-    application
-}
-
-java.toolchain {
-    languageVersion.set(JavaLanguageVersion.of(21))
-}
-
-repositories {
-    mavenCentral()
+    id("hu.bme.mit.ase.shingler.gradle.java")
 }
 
 val picoCliVersion = "4.7.6"
@@ -26,20 +16,4 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
 
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
-}
-
-tasks {
-    test {
-        useJUnitPlatform()
-        testLogging.showStandardStreams = true
-        finalizedBy(jacocoTestReport)
-    }
-
-    jacocoTestReport {
-        inputs.files(test.get().outputs)
-    }
-}
-
-application {
-    mainClass = "hu.bme.mit.ase.shingler.similarity.SimilarityApp"
 }
